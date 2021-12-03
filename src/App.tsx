f131b9a28e12c71b232052c5db917702d644a96d
Mike Chen <mhchen@gmail.com>
2021-12-03 15:02:29 -0800

    Blah

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+/*
+  <main class="main">
+    <div>
+      <button class="button" data-opener="offer">
+        Show offer
+      </button>
     </div>
+    <div>
+      <button class="button" data-opener="instructions">
+        Show instructions
+      </button>
+    </div>
+  </main>
+
+  <div class="modal-overlay" data-overlay="offer" hidden>
+    <div class="modal" role="alert" aria-role="alert">
+      <div class="modal__header">
+        <button class="button--icon" data-close="offer">
+          X
+        </button>
+      </div>
+      <div class="modal__body">
+        Click the button below to accept amazing offer!
+      </div>
+      <div class="modal__footer">
+        <button class="button" data-confirm="offer">
+          Accept offer
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <div class="modal-overlay" data-overlay="instructions" hidden>
+    <div class="modal" role="alert" aria-role="alert">
+      <div class="modal__header">
+        <div class="button--icon" data-close="instructions">
+          X
+        </div>
+      </div>
+      <div class="modal__body">
+        These are the instructions
+      </div>
+    </div>
+  </div>
+*/
+
+export default function App() {
+  return (
+  <main className="main" id="asdflkasjfdlkajsfdlkj" onClick={() => { console.log('hello') }} style={{ display: 'none' }} />
   );
 }
-
-export default App;
